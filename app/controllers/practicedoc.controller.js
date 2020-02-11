9e963fcaa8ed17ed00a8ffec8226cc462d57c89c
heiki50y <45164787+heiki50y@users.noreply.github.com>
2020-02-11 15:02:34 +0200

    try catch block

--- app/controllers/practicedoc.controller.js
+++ app/controllers/practicedoc.controller.js
@@ -2,123 +2,110 @@ const db = require("../models");
 const Practicedoc = db.practicedocs;
 const Op = db.Sequelize.Op;
 
-// Create and Save a new Practicedoc
-exports.createCompanyDoc = (req, res) => {
-  // Validate request
-  if (!req.body.praktika_ettevote) {
-    res.status(400).send({
-      message: "Content can not be empty!"
-    });
-    return;
+// Create Company data
+exports.createCompanyDoc = async (req, res, next) => {
+
+  try {
+    const company = await Practicedoc.create(req.body);
+
+    res.status(201).json({
+      sucesss: true,
+      data: company
+    })
+
+  } catch (err) {
+    next(err)
   }
-
-  // Create a Practicedoc
-  const practicedoc = {
-    
-    praktika_ettevote: req.body.praktika_ettevote,
-    praktika_telefon: req.body.praktika_telefon,
-    praktika_email: req.body.praktika_email,
-    juriidiline_aadress: req.body.juriidiline_aadress,
-    lepingusolmija_nimi: req.body.lepingusolmija_nimi,
-    lepingusolmija_amet: req.body.lepingusolmija_amet,
-    lepingu_alus: req.body.lepingu_alus,
-    juhendaja_nimi: req.body.juhendaja_nimi,
-    juhendaja_amet: req.body.juhendaja_amet,
-    juhendaja_telefon: req.body.juhendaja_telefon
-    
-  };
-
-  // Save Practicedoc in the database
-  Practicedoc.create(practicedoc)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the Practicedoc."
-      });
-    });
 };
 
-// Retrieve all Practicedocs from the database.
-exports.getAllCompanyDoc = (req, res) => {
-  const title = req.query.eriala;
-  var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+// Get all company data
+exports.getAllCompanyDoc = async (req, res, next) => {
+  try {
+    const company = await Practicedoc.findAll();
 
-  Practicedoc.findAll({ where: condition })
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || "Some error occurred while retrieving practicedocs."
-      });
+    res.status(200).json({ 
+      success: true, 
+      count: company.length, 
+      data: company
     });
+  } catch (err) {
+      next(err);
+  }
 };
 
-// Find a single Practicedoc with an id
-exports.getCompanyDoc = (req, res) => {
+// Find company
+exports.getCompanyDoc = async (req, res, next) => {
+  try {
+    const company = await Practicedoc.findByPk(req.params.id);
+
+    if(!company) {
+      return  next(
+          new ErrorResponse(`User not found with id of ${req.params.id}`, 404)
+      );
+    }
+
+      res.status(200).json({
+        success: true,
+        data: company 
+      });
+
+  } catch (err) {
+      next(err);
+  }
+};
+
+// Update Company
+exports.updateCompanyDoc = async (req, res, next) => {
   const id = req.params.id;
+  const company = await Practicedoc.findByPk(req.params.id);
+  try {
+    
+    if(!company) {
+      return  next(
+          new ErrorResponse(`User not found with id of ${req.params.id}`, 404)
+      );
+    }
 
-  Practicedoc.findByPk(id)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error retrieving Practicedoc with id=" + id
-      });
-    });
-};
+    await Practicedoc.update({
+      praktika_ettevote: req.body.praktika_ettevote,
+      praktika_telefon: req.body.praktika_telefon,
+      praktika_email: req.body.email,
+      juriidiline_aadress: req.body.juriidiline_aadress,
+      lepingusolmija_nimi: req.body.lepingusolmija_nimi,
+      lepingusolmija_amet: req.body.lepingusolmija_amet,
+      lepingu_alus: req.body.lepingu_alus,
+      juhendaja_nimi: req.body.juhendaja_nimi,
+      juhendaja_amet: req.body.juhendaja_amet,
+      juhendaja_telefon: req.body.juhendaja_telefon
 
-// Update a Practicedoc by the id in the request
-exports.updateCompanyDoc = (req, res) => {
-  const id = req.params.id;
+    }, { where: {id: id} })
+                                          
+    res.status(200).json({success: true, data: req.body});
 
-  Practicedoc.update(req.body, {
-    where: { id: id }
-  })
-    .then(num => {
-      if (num == 1) {
-        res.send({
-          message: "Practicedoc was updated successfully."
-        });
-      } else {
-        res.send({
-          message: `Cannot update Practicedoc with id=${id}. Maybe Practicedoc was not found or req.body is empty!`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error updating Practicedoc with id=" + id
-      });
-    });
+  } catch (err) {
+    next(err);
+  }
 };
 
 // Delete a Practicedoc with the specified id in the request
-exports.deleteCompanyDoc = (req, res) => {
+exports.deleteCompanyDoc = async (req, res, next) => {
   const id = req.params.id;
+  const company = await Practicedoc.findByPk(req.params.id);
+  try {
 
-  Practicedoc.destroy({
-    where: { id: id }
-  })
-    .then(num => {
-      if (num == 1) {
-        res.send({
-          message: "Practicedoc was deleted successfully!"
-        });
-      } else {
-        res.send({
-          message: `Cannot delete Practicedoc with id=${id}. Maybe Practicedoc was not found!`
-        });
-      }
+    if(!company) {
+      return  next(
+          new ErrorResponse(`User not found with id of ${req.params.id}`, 404)
+      );
+    }
+
+    await Practicedoc.destroy({
+      where: { id: id }
     })
-    .catch(err => {
-      res.status(500).send({
-        message: "Could not delete Practicedoc with id=" + id
-      });
-    });
+ 
+    res.status(200).json({success: true, data: {}});
+
+  } catch (err) {
+    next(err);
+  }
 };
